461ab1cb0799b3e705b81c0bb2cc177871f30f8b
limengxuan <limengxuan@4paradigm.com>
2023-12-13 19:56:37 +0800

    fix scripts

--- run_device_plugin.sh
+++
@@ -1,1 +0,0 @@
-/nvidia-device-plugin --resource-name=nvidia.com/gpu --mig-strategy=none --device-memory-scaling=1 --device-cores-scaling=1 --device-split-count=10 --disable-core-limit=false -v=false
